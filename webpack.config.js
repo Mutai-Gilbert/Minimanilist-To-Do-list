af0a84bef3d1f045d67d3ffc57bda29e0da23eca
mutai-gilbert <mutaigilbert85@gmail.com>
2022-12-09 21:50:21 +0300

    Added linters

--- webpack.config.js
+++ webpack.config.js
@@ -28,4 +28,17 @@ module.exports = {
   optimization: {
     runtimeChunk: 'single',
   },
+  settings: {
+    'import/resolver': {
+      alias: {
+        map: [
+          ['babel-polyfill', 'babel-polyfill/dist/polyfill.min.js'],
+          ['helper', './utils/helper'],
+          ['material-ui/DatePicker', '../custom/DatePicker'],
+          ['material-ui', 'material-ui-ie10']
+        ],
+        extensions: ['.ts', '.js', '.jsx', '.json']
+      },
+    },
+  },
 };
